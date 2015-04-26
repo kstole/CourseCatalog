f7632cce293492a1b3a8b48fc601ecccc1992f06
Rutger Farry <rutgerfarry@gmail.com>
2015-04-25 18:39:05 -0700

    Added Google Maps SDK

--- CourseCatalog/Pods/Target Support Files/Pods/Pods-environment.h
+++ CourseCatalog/Pods/Target Support Files/Pods/Pods-environment.h
@@ -12,3 +12,9 @@
 #define COCOAPODS_VERSION_MINOR_Alamofire 2
 #define COCOAPODS_VERSION_PATCH_Alamofire 1
 
+// Google-Maps-iOS-SDK
+#define COCOAPODS_POD_AVAILABLE_Google_Maps_iOS_SDK
+#define COCOAPODS_VERSION_MAJOR_Google_Maps_iOS_SDK 1
+#define COCOAPODS_VERSION_MINOR_Google_Maps_iOS_SDK 9
+#define COCOAPODS_VERSION_PATCH_Google_Maps_iOS_SDK 2
+
