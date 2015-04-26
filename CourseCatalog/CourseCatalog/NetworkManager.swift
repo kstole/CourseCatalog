801d9189b4aef0bf8bd0b69f63d06c9c5d6797ef
Kyler Stole <kylerstole@gmail.com>
2015-04-26 07:39:58 -0700

    Displays network activity indicator in status bar during API calls

--- CourseCatalog/CourseCatalog/NetworkManager.swift
+++ CourseCatalog/CourseCatalog/NetworkManager.swift
@@ -14,6 +14,8 @@ class NetworkManager {
     static let url = "http://54.213.102.221:80"
     
     class func getMajors(callback:(JSON) -> Void) -> Void {
+        let monitor = UIApplication.sharedApplication().networkActivityIndicatorVisible
+        UIApplication.sharedApplication().networkActivityIndicatorVisible = true
         Alamofire.request(.GET, "\(url)/majors/")
             .responseJSON { (_, _, response, error) in
                     if error == nil {
@@ -22,10 +24,15 @@ class NetworkManager {
                     } else {
                         println(error)
                     }
+                if !monitor {
+                    UIApplication.sharedApplication().networkActivityIndicatorVisible = false
+                }
             }
     }
 
     class func getClassesWithMajorId(majorId: String, term: String, callback:(JSON) -> Void) {
+        let monitor = UIApplication.sharedApplication().networkActivityIndicatorVisible
+        UIApplication.sharedApplication().networkActivityIndicatorVisible = true
         Alamofire.request(.GET, "\(url)/classes/", parameters: ["major_id": majorId, "term": term])
             .responseJSON { (_, _, response, error) in
                 if error == nil {
@@ -34,10 +41,15 @@ class NetworkManager {
                 } else {
                     println(error)
                 }
+                if !monitor {
+                    UIApplication.sharedApplication().networkActivityIndicatorVisible = false
+                }
             }
     }
     
     class func getClassDetailsWithId(id: String, callback:(JSON) -> Void) {
+        let monitor = UIApplication.sharedApplication().networkActivityIndicatorVisible
+        UIApplication.sharedApplication().networkActivityIndicatorVisible = true
         Alamofire.request(.GET, "\(url)/class_details/", parameters: ["class_id": id])
             .responseJSON { (_, _, response, error) in
                 if error == nil {
@@ -46,10 +58,15 @@ class NetworkManager {
                 } else {
                     println(error)
                 }
-        }
+                if !monitor {
+                    UIApplication.sharedApplication().networkActivityIndicatorVisible = false
+                }
+            }
     }
     
     class func getClassDetailsWithCRN(crn: String, callback:(JSON) -> Void) {
+        let monitor = UIApplication.sharedApplication().networkActivityIndicatorVisible
+        UIApplication.sharedApplication().networkActivityIndicatorVisible = true
         Alamofire.request(.GET, "\(url)/class_details/", parameters: ["class_crn": crn])
             .responseJSON { (_, _, response, error) in
                 if error == nil {
@@ -58,7 +75,10 @@ class NetworkManager {
                 } else {
                     println(error)
                 }
-        }
+                if !monitor {
+                    UIApplication.sharedApplication().networkActivityIndicatorVisible = false
+                }
+            }
     }
 
 }
