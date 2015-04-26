5a5e340a79051cd94e44963d89a0700b9aa7c742
Rutger Farry <rutgerfarry@gmail.com>
2015-04-25 18:33:40 -0700

    Added NetworkManager

---
+++ CourseCatalog/CourseCatalog/NetworkManager.swift
@@ -0,0 +1,18 @@
+//
+//  NetworkManager.swift
+//  CourseCatalog
+//
+//  Created by Rutger Farry on 4/25/15.
+//  Copyright (c) 2015 Kyler Stole. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class NetworkManager {
+    let url = "http://54.213.102.221:80"
+    
+    func getMajors() -> [Dictionary<String, String>] {
+        //Alamofire.request(.GET, url, parameters: <#[String : AnyObject]?#>, encoding: <#ParameterEncoding#>)
+    }
+}
