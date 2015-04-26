d952aec97a945b84b772b4edcc6a0fa5af199682
Kyler Stole <kylerstole@gmail.com>
2015-04-26 07:17:20 -0700

    Fixed warning messages for implicit conversion in fabsf() by converting to fabs()

--- CourseCatalog/Pods/pop/pop/POPDecayAnimationInternal.h
+++ CourseCatalog/Pods/pop/pop/POPDecayAnimationInternal.h
@@ -58,8 +58,9 @@ struct _POPDecayAnimationState : _POPPropertyAnimationState
     CGFloat f = dynamicsThreshold * kPOPAnimationDecayMinimalVelocityFactor;
     const CGFloat *velocityValues = vec_data(velocityVec);
     for (NSUInteger idx = 0; idx < valueCount; idx++) {
-      if (fabsf(velocityValues[idx]) >= f)
-        return false;
+        if (fabs(velocityValues[idx]) >= f) {
+            return false;
+        }
     }
     return true;
 
