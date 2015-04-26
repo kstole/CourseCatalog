bfcf53438a87a5f639c524e6658daf5dc7dbc32b
Rutger Farry <rutgerfarry@gmail.com>
2015-04-25 22:58:34 -0700

    Modified lanuch screen

--- CourseCatalog/CourseCatalog/AppDelegate.swift
+++ CourseCatalog/CourseCatalog/AppDelegate.swift
@@ -16,8 +16,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
-        
         // Configure Navigation bar
         UINavigationBar.appearance().barTintColor = UIColor.OSUOrangeColor()
         UINavigationBar.appearance().tintColor = UIColor.whiteColor()
