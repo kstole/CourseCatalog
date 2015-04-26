10fdfe742508207fa5209b8e5fbd3003e7fb2b73
gungorbasa <gungorbasa@gmail.com>
2015-04-26 02:38:49 -0700

    I dont know. It is too late

--- CourseCatalog/CourseCatalog/ClassList.swift
+++ CourseCatalog/CourseCatalog/ClassList.swift
@@ -7,11 +7,34 @@
 //
 
 import Foundation
+import SwiftyJSON
 
-func saveClass(id:String, json:NSDictionary) {
-    NSUserDefaults.standardUserDefaults().setObject(json, forKey: id)
+class ClassList {
+    class func saveClass(id:String, json:JSON) {
+        var arr : NSArray = [json["class_name"].stringValue, json["description"].stringValue, json["days_of_week"].stringValue, json["start_time"].stringValue, json["end_time"].stringValue, json["professor"]["first_name"].stringValue, json["professor"]["last_name"].stringValue, json["professor"]["rating"].stringValue, (json["lat"].stringValue as NSString), (json["lon"].stringValue as NSString)]
+        NSUserDefaults.standardUserDefaults().setObject(arr, forKey: "cl" + id)
+        NSUserDefaults.standardUserDefaults().synchronize()
+    }
+    
+    class func loadClassList() -> NSArray {
+        var arr : NSArray = NSUserDefaults.standardUserDefaults().dictionaryRepresentation().keys.array
+        var resulArr = [NSArray]()
+        var i : Int
+        for index in 0 ... arr.count {
+            if (arr[index].hasPrefix("cl")) {
+                //resulArr.append(NSUserDefaults.standardUserDefaults().objectForKey(arr[index]) as! NSArray)
+            }
+        }
+        return NSUserDefaults.standardUserDefaults().dictionaryRepresentation().values.array
+    }
+    
+    class func isAdded(id:String) -> Bool {
+        if (NSUserDefaults.standardUserDefaults().stringForKey(id as String) == nil) {
+            return false
+        }
+        return true
+    }
 }
 
-func loadClassList() -> NSArray {
-    return NSUserDefaults.standardUserDefaults().dictionaryRepresentation().values.array
-}
+
+
--- CourseCatalog/CourseCatalog/CourseViewController.swift
+++ CourseCatalog/CourseCatalog/CourseViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import GoogleMaps
+import SwiftyJSON
 
 class CourseViewController: UITableViewController, CLLocationManagerDelegate  {
     
@@ -22,6 +23,7 @@ class CourseViewController: UITableViewController, CLLocationManagerDelegate  {
     
     @IBOutlet weak var map: UIView!
     //var mapView : GMSMapView!
+    @IBOutlet weak var addButton: UIBarButtonItem!
     
     @IBOutlet weak var course_description_cell: UITableViewCell!
 
@@ -34,11 +36,14 @@ class CourseViewController: UITableViewController, CLLocationManagerDelegate  {
         }
     }
     
+    var result:JSON!
+    
     var dstLat:Double!
     var dstLon:Double!
     
     var userLat:Double!
     var userLon:Double!
+    var classId:String!
 
     let locationManager = CLLocationManager()
     
@@ -57,11 +62,20 @@ class CourseViewController: UITableViewController, CLLocationManagerDelegate  {
         // Do any additional setup after loading the view, typically from a nib.
         self.configureView()
         
+        
+        
+        //#pragma warning deletes all NSUserDefaults
+        NSUserDefaults.standardUserDefaults().removePersistentDomainForName(NSBundle.mainBundle().bundleIdentifier!)
+        
         tableView.estimatedRowHeight = 44.0
         tableView.rowHeight = UITableViewAutomaticDimension
+        classId = "35"
         
-        NetworkManager.getClassDetailsWithId("25") { (json) -> Void in
+        
+        NetworkManager.getClassDetailsWithId(classId) { (json) -> Void in
             println(json)
+            
+            self.result = json
             
             self.course_name.text = json["class_name"].stringValue
 
@@ -78,6 +92,16 @@ class CourseViewController: UITableViewController, CLLocationManagerDelegate  {
             self.tableView.reloadData()
             
         }
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        if (ClassList.isAdded(classId)) {
+            self.addButton.enabled = false
+        }
+        else {
+            self.addButton.enabled = true
+        }
+        
     }
     
     
@@ -152,5 +176,14 @@ class CourseViewController: UITableViewController, CLLocationManagerDelegate  {
         locationManager.stopUpdatingLocation()
     }
 
+    @IBAction func addClass(sender: AnyObject) {
+        if (self.result != nil) {
+            ClassList.saveClass(classId, json: self.result)
+            self.addButton.enabled = false
+        }
+        else {
+            self.addButton.enabled = true
+        }
+    }
 }
 
